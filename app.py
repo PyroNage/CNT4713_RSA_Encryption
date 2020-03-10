163d2d52a2fe1e4c9f02051710b38721fd76fa24
PyroNage <marcodambrosio96@gmail.com>
2020-03-09 22:51:26 -0400

    Using the RSA Example given i have implemented a basic encryption and decryption website. Public and Private keys are stored in the app.py so it is probably not the most secure but for an MVP this works. Commiting so i can return the results as an object and display on the table in the index.html

--- app.py
+++ app.py
@@ -4,6 +4,9 @@
 # redirect is for redirecting to different (or the same) route
 from flask import Flask, render_template, url_for, request, redirect
 
+# Import random for the RSA Encryption
+import random
+
 # Defining flask app name
 app = Flask(__name__)
 
@@ -15,6 +18,56 @@ app = Flask(__name__)
 plainTextValue = ''
 cipherTextValue = ''
 decipheredTextValue = ''
+
+
+# Code to encrypt will be placed here
+
+# Algorithm from: https://crypto.stackexchange.com/questions/19444/rsa-given-q-p-and-e
+# Algorithm takes in e and phi_of_n respectively and returns d
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+        gcd = b
+    return x
+
+
+def encrypt(pk, plaintext):
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def join_encrypted(ciphertext):
+    # Generate the string using the values of the ciphertext list
+    plain = [chr(char) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+# Parameters for encryption to be defined below
+p = 47                          # p
+q = 59                          # q
+n = p * q                       # n
+phi_of_n = (p - 1) * (q - 1)    # phi
+e = 17                          # e was chosen by the video i was watching as a number that worked.
+d = egcd(e, phi_of_n)           # find d from e * d mod phi_of_n = 1
+publicKey = (e, n)              # public key tuple of value e and n
+privateKey = (d, n)             # private key tuple of value d and n
 
 
 # Main Route, defining the ability to make GET and POST requests to this main route
@@ -36,30 +89,22 @@ def index():
     # If the request is NOT a POST (so if the request is a GET)
     else:
         # return render_template('index.html', users=items)only showing this code to show how to pass parameters to html
- 
+
         return render_template('index.html')
-
-
-def encrypt_data(data_to_encrypt):
-    print(data_to_encrypt)
-
-
-def decrypt_data(data_to_decrypt):
-    print(data_to_decrypt)
 
 
 # If the user clicks on the encrypt form, the app makes a post request to this route
 @app.route('/encrypt', methods=['POST', 'GET'])
-def encrypt():
-    encrypt_data(request.form['plainText'])
-    return 'encrypt: %s' % request.form['plainText']
+def encrypt_route():
+    encrypted_text = encrypt(publicKey, request.form['plainText'])
+    return 'Encrypted text: %s' % join_encrypted(encrypted_text)
 
 
 # If the user clicks on the decrypt form, the app makes a post request to this route
 @app.route('/decrypt', methods=['POST', 'GET'])
-def decrypt():
-    decrypt_data(request.form['cipherText'])
-    return 'decrypt: %s' % request.form['cipherText']
+def decrypt_route():
+    decrypted_text = encrypt(privateKey, request.form['cipherText'])
+    return 'Decrypted text: %s' % join_encrypted(decrypted_text)
 
 
 # @app.route('/delete/<string:email>') Showing this to show how to pass parameters to the route
